b7ce6f5647f05927b762dbc2689a43d2275365ca
Elaine Auman <ashlynauman@gmail.com>
2020-09-16 09:08:50 -0600

    second push

--- prototype 1 driving sim/Assets/Scripts/PlayerController1.cs
+++ prototype 1 driving sim/Assets/Scripts/PlayerController1.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController1 : MonoBehaviour
 {
     public float speed = 20;
+    public float turnSpeed;
+    
+    public float horizontalInput;
+    public float verticalInput;
     
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,11 @@ public class PlayerController1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
         // moves the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
