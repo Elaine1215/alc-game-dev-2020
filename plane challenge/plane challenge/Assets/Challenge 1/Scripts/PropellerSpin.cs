0b206043d8587335c9c7f149f2af7f035d144ccc
Elaine Auman <ashlynauman@gmail.com>
2020-09-24 09:11:19 -0600

    plane

---
+++ plane challenge/plane challenge/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    public Vector3 RotateAmount = 10; // degrees per second to rotate in each axis. Set in inspector
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotateAmount * Time.deltaTime);
+    }
+}
