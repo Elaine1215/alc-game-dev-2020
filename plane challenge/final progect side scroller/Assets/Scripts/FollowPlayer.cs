8b9be4cde6a31178365a3ff374430b13df0a9530
Elaine Auman <ashlynauman@gmail.com>
2020-10-22 09:08:37 -0600

    start challenge 2

---
+++ plane challenge/final progect side scroller/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    //set what player is
+    public GameObject Player;
+
+    //adds angle to camera
+    public Vector3 offset = new Vector3(40,10,15);
+
+    // Update is called once per frame
+    void Update()
+    {
+        //makes camera follow player
+        transform.position = Player.transform.position + offset;
+    }
+}
