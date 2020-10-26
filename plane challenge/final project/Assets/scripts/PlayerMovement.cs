56485657febd7f87be9ca85336ff3a39ae225bbb
Elaine Auman <ashlynauman@gmail.com>
2020-10-26 09:10:46 -0600

    maybe i fixed it, maybe not

---
+++ plane challenge/final project/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //sets speed
+    private float speed = 20.0f;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gets the needed directions
+        verticalInput = Input.GetAxis("Vertical");
+
+        //moves player forward and backward
+        transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+}
