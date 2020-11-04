d196cbe10bf8d9beb102144422cb2da15798df0c
Elaine Auman <ashlynauman@gmail.com>
2020-11-04 09:04:57 -0700

    Spawn manager

--- plane challenge/final project/Assets/scripts/PlayerMovement.cs
+++ plane challenge/final project/Assets/scripts/PlayerMovement.cs
@@ -8,19 +8,37 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 20.0f;
 
     private float horizontalInput;
-    private float verticalInput;
 
+    //jumping
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityMod;
+    public bool isOnGround = true;
+
+    private void Start()
+    {
+        //initialize component and add rigidbody
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
+    }
     // Update is called once per frame
     void Update()
     {
         //gets the needed directions
         horizontalInput = Input.GetAxis("Horizontal");
-        //get keys to make player jump
-        verticalInput = Input.GetAxis("");
 
         //moves player forward and backward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
-        //make player jump
 
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
---
+++ plane challenge/run jump/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    //speed of object
+    public float speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moves game object left at set speed
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
