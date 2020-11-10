83de41151e43edac892fcbbb71cfb03069adb8fd
Elaine Auman <ashlynauman@gmail.com>
2020-11-10 09:09:45 -0700

    who knows

--- plane challenge/run jump/Assets/Scripts/PlayerController.cs
+++ plane challenge/run jump/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float gravityMod;
     public bool isOnGround = true;
     public bool gameOver = false;
+    //allows access to animations
+    private Animator playerAnim;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,8 @@ public class PlayerController : MonoBehaviour
         //initialize component and add rigidbody
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityMod;
+        //initialize animation
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,8 +27,11 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
+            //makes player jump and keeps from double jumping
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            //pulls down jumping animation
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -34,10 +41,12 @@ public class PlayerController : MonoBehaviour
 
         if(collision.gameObject.CompareTag("Ground"))
         {
+            //allows jumping only if on the ground
             isOnGround = true;
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            //ends game
             gameOver = true;
             Debug.Log("Game Over!");
         }
