e0eb0e6cb5494d78ec0ed0054d83d41ea7682740
Elaine Auman <ashlynauman@gmail.com>
2021-01-12 08:29:16 -0700

    challenge 5 mistake

--- plane challenge/wack a food/Assets/Challenge 5/Scripts/GameManagerX.cs
+++ plane challenge/wack a food/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -22,6 +22,7 @@ public class GameManagerX : MonoBehaviour
     private float spaceBetweenSquares = 2.5f; 
     private float minValueX = -3.75f; //  x value of the center of the left-most square
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
+    private float timeLeft;
     
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
     public void StartGame(int difficulty)
@@ -87,6 +88,18 @@ public class GameManagerX : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    private void Update()
+    {
+        if (isGameActive)
+        {
+            timeLeft -= Time.deltaTime;
+            timerText.SetText("Time: " + Mathf.Round(timeLeft));
+            if (timeLeft < 0)
+            {
+                GameOver();
+            }
+        }
     }
 
 }
